9473d6666c0ca143f52781efc41288acd8ecb11c
Yiyi Wang <shd101wyy@gmail.com>
2026-03-18 21:40:22 +0800

    Revert "fix: Fix devenv on macOS (darwin)"
    This reverts commit 51554ef8b39228afd34acac35cf96decc54426db.

--- devenv.nix
+++ devenv.nix
@@ -14,10 +14,9 @@
     protobuf
     cmake
     zlib
-    onnxruntime
-  ];
+  ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [ pkgs.onnxruntime ];
 
-  enterShell = ''
+  enterShell = pkgs.lib.optionalString pkgs.stdenv.isLinux ''
     export ORT_LIB_LOCATION="${pkgs.onnxruntime}/lib"
     export ORT_PREFER_DYNAMIC_LINK=1
     export LD_LIBRARY_PATH="${pkgs.onnxruntime}/lib:$LD_LIBRARY_PATH"
